99eb7cd6c52ed1a65eb26adef43de268cacd340f
BanGo <695371519@qq.com>
2025-03-14 14:24:35 +0800

    init uboot2

---
+++ u-boot/include/configs/suniv.h
@@ -0,0 +1,21 @@
+/*
+ * Configuration settings for new Allwinner F-series (suniv) CPU
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#define CONFIG_SUNXI_USB_PHYS 1
+
+/*
+ * Include common sunxi configuration where most the settings are
+ */
+#include <configs/sunxi-common.h>
+
+#define CONFIG_VIDEO_LOGO
+#define CONFIG_VIDEO_BMP_LOGO
+#define CONFIG_BOOTCOMMAND   "run distro_bootcmd"
+
+#endif /* __CONFIG_H */
